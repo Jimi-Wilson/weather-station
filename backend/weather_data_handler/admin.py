b4a383b231738498a7ea0cf0404dc3922d54671c
Jimi Wilson <jimiwilson24@gmail.com>
2025-08-26 02:12:01 +0100

    Updated admin registration

--- backend/weather_data_handler/admin.py
+++ backend/weather_data_handler/admin.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
+from rest_framework_api_key.admin import APIKeyModelAdmin
 
-from .models import Reading
+from .models import Reading, UploadBatch, WeatherStation, WeatherStationAPIKey
 
 
 @admin.register(Reading)
+class ReadingAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(UploadBatch)
+class UploadBatchAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(WeatherStation)
 class WeatherStationAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(WeatherStationAPIKey)
+class WeatherStationAPIKeyAdmin(APIKeyModelAdmin):
     pass
